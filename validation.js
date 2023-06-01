96f43544fd868fdd266402a239a884a85921a0f0
harikrishnakavuri123 kavuri <harikrishnakavuri123@gmail.com>
2023-06-01 07:00:47 +0000

    kavurihari

--- validation.js
+++ validation.js
@@ -32,53 +32,124 @@ function writeDataToFirebase(db, path, data) {
       });
     });
 }
+function removeSpecialCharacters(str) {
+  const regex = /[^a-zA-Z0-9 ]/g;
+  return str.replace(regex, '');
+}
 function signUp()
 {
-    var name = document.getElementById('name').value;
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var answer = document.getElementById('security-question').value;
-    writeDataToFirebase(db, name, {
-        "username": username,
-        "password": password,
-        "answer" : answer
-      }).then(() => {
-        alert('Signed up Successfully! Login Please');
-        window.open('login.html','_self');
-      }).catch(error => {
-        alert(error);
-      });
+  var name = document.getElementById('name').value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var conpassword = document.getElementById('confirm-password').value;
+  var answer = document.getElementById('security-question').value;
+  var nonEmptyFields = (name != '' && username != '' && password != '' && answer != '' && conpassword != '')
+  var passwordvalidation = (password === conpassword)
+  var path = removeSpecialCharacters(username);
+
+  if(nonEmptyFields && passwordvalidation)
+  {
+    writeDataToFirebase(db, path, {
+      "name": name,
+      "username": username,
+      "password": password,
+      "answer" : answer
+    }).then(() => {
+      alert('Signed up Successfully! Login Please');
+      window.open('index.html','_self');
+    }).catch(error => {
+      alert(error);
+    });
+  }
+  else
+  {
+    if(nonEmptyFields==false)
+    {
+      alert('All the fields are required for Signing up!');
+    }
+    else
+    {
+      alert('Sorry, Passwords did not match!');
+    }
+  }
 }
 function resetPassword() 
 {
-      var username = document.getElementById('username').value;
-      var password = document.getElementById('password').value;
-
-      if (username === "" || password === "") {
-        alert("Please enter your username and password.");
-        return;
-      }
-      var firebase = firebase.initializeApp();
-      var auth = firebase.auth();
-      auth.sendPasswordResetEmail(username).then(function() {
-        alert("A password reset email has been sent to your email address.");
-      }, function(error) {
-        alert("An error occurred while resetting your password.");
-      });
-}
-
-
-function signIn() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  
-  checkEmailAndPassword(username, password).then(function(exists) {
-    if (exists) {
-      alert("You are signed in!");
-      window.location.href = "/";
-    } else {
-      alert("The email or password is incorrect.");
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var conpassword = document.getElementById('confirm-password').value;
+  var answer = document.getElementById('security-question').value;
+  var nonEmptyFields = (username != '' && password != '' && answer != '' && conpassword != '')
+  var passwordvalidation = (password === conpassword)
+  if(nonEmptyFields && passwordvalidation)
+  {
+    var path = removeSpecialCharacters(username)
+    var ref = db.ref(path);
+    ref.once("value")
+      .then(function(snapshot) {
+        var data = snapshot.val();
+        console.log(data);
+        console.log(data.answer);
+        console.log(data.answer == answer);
+        if(data.answer == answer)
+        {
+          writeDataToFirebase(db, path, {
+            "name": name,
+            "username": username,
+            "password": password,
+            "answer" : answer
+          }).then(() => {
+            alert('Password reset successfully!');
+            window.open('index.html','_self');
+          }).catch(error => {
+            alert(error);
+          });
+        }
+      })
+      .catch(function(error) {
+        console.error(error);
+        alert(error);
+      });      
+  }
+  else
+  {
+    if(nonEmptyFields==false)
+    {
+      alert('All the fields are required for Signing up!');
     }
-  });
-  
+    else
+    {
+      alert('Sorry, Passwords did not match!');
+    }
+  }
 }
+
+function getUserCredentials()
+{
+const ref = db.ref('/');
+ref.on('value', function(snapshot) {
+  data = snapshot.val();
+  console.log(data);
+});
+}
+async function logIn()
+{
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var path = removeSpecialCharacters(username)
+  var ref = db.ref(path);
+  ref.once("value")
+    .then(function(snapshot) {
+      var data = snapshot.val();
+      console.log(data.password);
+      console.log(data.password == password);
+      if(data.password == password)
+      {
+        window.open('landing_page.html','_self');
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+    }); 
+}
+
